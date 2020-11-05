2b55fdc6043a10e1bcd0947a7631d6436c6e71c8
osmajosue <osmadc005@gmail.com>
2020-11-05 16:01:13 -0400

    Organize Helpers class

--- src/test/java/Utils/Helpers.java
+++ src/test/java/Utils/Helpers.java
@@ -6,7 +6,6 @@ public class Helpers {
     public static String BASE_URL = "https://twitter.com/";
     public static String FAKE_EMAIL_SCRIPT = "window.open('https://www.fakemail.net/')";
     public static String ACCOUNT_NAME = "TESTINGUSER478";
-    public static int NAME_LENGTH = ACCOUNT_NAME.length();
     public static String ACCOUNT_MONTH = "May";
     public static String ACCOUNT_DAY = "14";
     public static String ACCOUNT_YEAR = "1998";
@@ -15,7 +14,9 @@ public class Helpers {
     public static String TEXT_TWEET = "I AM A SPACE MAN";
     public static String TEXT_IMAGE = "THIS IS A TEST PHOTO";
     public static String TEXT_VIDEO = "THIS IS A TEST VIDEO";
-    public static String IMAGE_SRC = "C:\\Users\\Osmani Diaz\\IdeaProjects\\Techaid_Automation_Challenge\\Media\\Test_image.jpg";
-    public static String VIDEO_SRC = "C:\\Users\\Osmani Diaz\\IdeaProjects\\Techaid_Automation_Challenge\\Media\\Test_video1.mp4";
+    public static String ROOT_DIR = "C:\\Users\\Osmani Diaz\\IdeaProjects\\Techaid_Automation_Challenge"; //This needs to be the project root directory
+    public static String IMAGE_SRC = ROOT_DIR + "/Media/Test_image.jpg";
+    public static String VIDEO_SRC = ROOT_DIR + "Media/Test_video1.mp4";
     public static String LINK_TWEET = ":\r\n https://www.sciencenews.org/";
+    public static int NAME_LENGTH = ACCOUNT_NAME.length();
 }
